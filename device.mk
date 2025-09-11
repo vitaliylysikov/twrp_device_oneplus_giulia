0bb4c6c11a9337df92f0451bc0e06a87b8945eec
vitaliylysikov <84893623+vitaliylysikov@users.noreply.github.com>
2025-09-12 07:35:38 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,20 +4,29 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Configure base.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Configure core_64_bit_only.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Configure virtual_ab_ota compression_with_xor.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression_with_xor.mk)
+
+# Configure emulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Configure twrp config common.mk
+$(call inherit-product, vendor/twrp/config/common.mk)
+
 # Shipping API level
-BOARD_SHIPPING_API_LEVEL := 36
-PRODUCT_SHIPPING_API_LEVEL := 36
-PPRODUCT_TARGET_VNDK_VERSION := 36
+BOARD_SHIPPING_API_LEVEL := 35
+PRODUCT_SHIPPING_API_LEVEL := 35
+PPRODUCT_TARGET_VNDK_VERSION := 35
 
 # Kernel
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 PRODUCT_ENABLE_UFFD_GC := true
-
-# Fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery \
-    fastbootd
 
 # Sys flags
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -33,7 +42,3 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.fuse.passthrough.enable=true
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
-
-# Touch features
-$(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_GM,true)
-$(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_HTPR,false)

2609f74ea83cd88f9fbb9fc6a14ce9a577b66a11
vitaliylysikov <84893623+vitaliylysikov@users.noreply.github.com>
2025-09-16 23:05:38 +0800

    Add files via upload

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2020-2025 The OrangeFox Recovery Project
+#
+	export ALLOW_MISSING_DEPENDENCIES=true
+	export LC_ALL="C"
+
+    #OFR build settings & info
+    export TARGET_DEVICE_ALT="PKG110,OP5D2BL1,CPH2645,OP5D3BL1,CPH2647,OP5D3BL1,CPH2691,OP5D3BL1"
